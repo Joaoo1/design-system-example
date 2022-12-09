0228cc880b46efefd99c7bd98b13b5c7e15d876a
João V <silva.joao1@outlook.com.br>
2022-12-09 01:20:37 -0300

    feat: text component

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,15 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Box, BoxProps } from '@monorepo/react'
+import { Box, BoxProps, Text } from '@monorepo/react'
 
 export default {
   title: 'Surfaces/Box',
   component: Box,
   args: {
-    children: (
-      <>
-        <span>Testando o elemento Box</span>
-      </>
-    ),
+    children: <Text>Testando o elemento Box</Text>,
   },
 } as Meta<BoxProps>
 
